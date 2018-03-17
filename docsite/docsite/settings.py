4688b4def8091f65b8dad10aeb55b32c7e158f50
pooja saraf <poojasaraf97@gmail.com>
2018-03-17 22:45:59 +0530

    second commit

--- docsite/docsite/settings.py
+++ docsite/docsite/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.apps.FillformConfig',
+    'fillform',
+    'home',
 ]
 
 MIDDLEWARE = [
@@ -76,10 +77,17 @@ WSGI_APPLICATION = 'docsite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'form_details',
+        'USER' : 'root',
+        'PASSWORD' : 'papaya1997',
+        'HOST' : 'localhost',
+        'PORT' : '8000',
+
+
     }
 }
+
 
 
 # Password validation
--- docsite/fillform/models.py
+++ docsite/fillform/models.py
@@ -1,3 +1,12 @@
+from __future__ import unicode_literals
 from django.db import models
 
-# Create your models here.
+class fill(models.Model):
+     title = models.CharField( max_length=100)
+     causes = models.TextField(default="")
+     symptoms = models.TextField(blank="")
+     diagnosis = models.TextField(blank="")
+     complications = models.TextField(blank="")
+     medicine = models.TextField(blank="")
+     upvote=models.IntegerField(default=0)
+     downvote=models.IntegerField(default=0)
